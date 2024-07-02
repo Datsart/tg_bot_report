d44fd9f1f95c4e688bed764e73542347813982f0
artemdatsenko <dacenko.aa@yandex.ru>
2024-07-02 20:22:35 +0300

    2

--- app.py
+++ app.py
@@ -11,5 +11,3 @@ def login():
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8000, debug=True)
-    # ssl_context = (os.path.join('', 'ssl', 'fullchain.pem'), os.path.join('', 'ssl', 'privkey.pem'))
-    # app.run(host='0.0.0.0', port=5000, ssl_context=ssl_context)
--- bot2.py
+++ bot2.py
@@ -1,25 +1,6 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from aiogram import Bot, Dispatcher, types
+from aiogram.types.web_app_info import WebAppInfo
 
-# Вставьте свой токен здесь
-TOKEN = '7147479868:AAEIJKh9iq4dApecwsUbUKQUAPzOW3_ja8E'
+bot = Bot('7147479868:AAEIJKh9iq4dApecwsUbUKQUAPzOW3_ja8E')
+dp = Dispatcher(bot)
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Вот ссылка на наше мини-приложение: https://your-domain.com')
-
-def main():
-    # Создаем экземпляр Updater и передаем ему токен вашего бота
-    updater = Updater(token=TOKEN, use_context=True)
-
-    # Получаем диспетчера для регистрации обработчиков
-    dispatcher = updater.dispatcher
-
-    # Регистрируем обработчики команд
-    dispatcher.add_handler(CommandHandler("start", start))
-
-    # Запускаем бота
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
