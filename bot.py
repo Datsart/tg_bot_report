c3de70b8c40953e2f2fbf389a7b01896842035ad
artemdatsenko <dacenko.aa@yandex.ru>
2024-07-02 23:14:51 +0300

    4

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from settings import Settings
 token = f'{Settings.tg_bot_token}'
 
 bot = telebot.TeleBot(token)
+bot.remove_webhook()
 
 # Список разрешенных пользователей, указываем ник без собаки!!!
 ALLOWED_USERS = [f'{Settings.my_name}']
