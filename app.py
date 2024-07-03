8e53d8b2756f3c0d19de54a45bb7b6742adb3faa
artemdatsenko <dacenko.aa@yandex.ru>
2024-07-03 18:56:07 +0300

    auth

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__, static_folder='static', template_folder='static')
 
 @app.route('/app', methods=['GET', 'POST'])
 def login():
-    return render_template('index.html')
+    list_users = ['datsenko_artem123']
+    return render_template('index.html', list_users=list_users)
 
 
 # Маршрут для файла app.js
