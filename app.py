0d8332ec66b806b5e9ea26d2ec1840f5e151521b
artemdatsenko <dacenko.aa@yandex.ru>
2024-07-03 15:04:10 +0300

    1 и 2 страница фронта

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, template_folder='./static', static_folder='./static')
+app = Flask(__name__, static_folder='static', template_folder='static')
 
 
 @app.route('/app', methods=['GET', 'POST'])
@@ -9,5 +8,11 @@ def login():
     return render_template('index.html')
 
 
+# Маршрут для файла app.js
+@app.route('/app.js')
+def serve_js():
+    return send_from_directory('.', 'app.js')
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)), debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
