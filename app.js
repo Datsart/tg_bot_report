0e8dd62427736a6c51b9fa5811dd05b2a3e9b604
artemdatsenko <dacenko.aa@yandex.ru>
2024-07-04 14:09:23 +0300

    auth

--- app.js
+++ app.js
@@ -1,37 +1,32 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    document.getElementById('second_menu').style.display = 'none';
-    let buttons_first_menu = document.querySelectorAll('#first_menu .btn');
+    document.getElementById('second_menu').style.display = 'none'
+    let buttons_first_menu = document.querySelectorAll('#first_menu .btn')
     buttons_first_menu.forEach(function (button) {
         button.addEventListener('click', function () {
-            document.getElementById('second_menu').style.display = 'block';
-            document.getElementById('first_menu').style.display = 'none';
-        });
-    });
+            document.getElementById('second_menu').style.display = 'block'
+            document.getElementById('first_menu').style.display = 'none'
 
-    // Получаем id пользователя - это и есть chat_id
-    let tg = window.Telegram.WebApp;
-    let userId = tg.initDataUnsafe.user.id;
-    document.getElementById('info').innerText = userId;
 
-    // Проверка значений в консоли
-    console.log("User ID: ", userId);
+        })
+    })
+    // получаем id пользователя - это и есть chat_id
+    let tg = window.Telegram.WebApp
+    document.getElementById('info').innerText = tg.initDataUnsafe.user.id
 
-    // Отправка данных в бэк при клике на тело документа
-    let url_for_post = "http://random-subdomain.ngrok.io/post_response"; // Замените на ваш ngrok URL
-    let data = { id: userId }; // Помещаем id в объект
-
-    document.querySelector('body').addEventListener('click', function () {
+    // отправка данных в бэк
+    let url_for_post_element = document.getElementById('url-for-post');
+    let url_for_post = url_for_post_element.getAttribute('data-url-for-post');
+    console.log(url_for_post)
+    let data = tg.initDataUnsafe.user.id
+    document.getElementById('btn1').addEventListener('click', function () {
         fetch(url_for_post, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data),
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Response from server:', data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
+        "method": "POST",
+        "headers": {"Content-Type": "application/json"},
+        "body": JSON.stringify(data),
+    })
+        .then(response => {
+            return response.json();
         });
-    });
-});
+    })
+
+})
