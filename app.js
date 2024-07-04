d7116ef3d8841666c654d78bd98c16cda840dd25
artemdatsenko <dacenko.aa@yandex.ru>
2024-07-04 15:19:59 +0300

    123

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
-document.addEventListener('DOMContentLoaded', function (event) {
-    document.getElementById('second_menu').style.display = 'none'
-    let buttons_first_menu = document.querySelectorAll('#first_menu .btn')
-    buttons_first_menu.forEach(function (button) {
-        button.addEventListener('click', function () {
-            document.getElementById('second_menu').style.display = 'block'
-            document.getElementById('first_menu').style.display = 'none'
-        })
-    })
-    document.getElementById('btn8').addEventListener('click', function () {
+const tg = window.Telegram.WebApp
+const user_name = tg.initDataUnsafe.user.displayName
+document.getElementById('info').innerText = tg.initDataUnsafe.user.displayName
+document.getElementById('info2').innerText = tg.initDataUnsafe.user.name
+
+let list_users_element = document.getElementById('list-users');
+let list_users = list_users_element.getAttribute('data-list-users');
+if (user_name in list_users) {
+    document.addEventListener('DOMContentLoaded', function (event) {
+
         document.getElementById('second_menu').style.display = 'none'
-        document.getElementById('first_menu').style.display = 'block'
+        let buttons_first_menu = document.querySelectorAll('#first_menu .btn')
+        buttons_first_menu.forEach(function (button) {
+            button.addEventListener('click', function () {
+                document.getElementById('second_menu').style.display = 'block'
+                document.getElementById('first_menu').style.display = 'none'
+            })
+        })
+        document.getElementById('btn8').addEventListener('click', function () {
+            document.getElementById('second_menu').style.display = 'none'
+            document.getElementById('first_menu').style.display = 'block'
+        })
+        // получаем id пользователя - это и есть chat_id
+        // const user_name = tg.initDataUnsafe.user.displayName
+
+        document.getElementById('info').innerText = tg.initDataUnsafe.user.displayName
+
+        // отправка данных в бэк
+        let url_for_post_element = document.getElementById('url-for-post');
+        let url_for_post = url_for_post_element.getAttribute('data-url-for-post');
+        console.log(url_for_post)
+        let data = tg.initDataUnsafe.user.id
+        fetch(url_for_post, {
+            "method": "POST",
+            "headers": {"Content-Type": "application/json"},
+            "body": JSON.stringify(data),
+        })
+            .then(response => {
+                return response.json();
+            });
+
+
     })
-    // получаем id пользователя - это и есть chat_id
-    let tg = window.Telegram.WebApp
-    document.getElementById('info').innerText = tg.initDataUnsafe.user.id
-
-    // отправка данных в бэк
-    let url_for_post_element = document.getElementById('url-for-post');
-    let url_for_post = url_for_post_element.getAttribute('data-url-for-post');
-    console.log(url_for_post)
-    let data = tg.initDataUnsafe.user.id
-    fetch(url_for_post, {
-        "method": "POST",
-        "headers": {"Content-Type": "application/json"},
-        "body": JSON.stringify(data),
-    })
-        .then(response => {
-            return response.json();
-        });
-
-
-})
+}
