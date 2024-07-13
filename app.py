e3f7e54c4fa9c14efe2786d8d3d8708d74fafcbd
artemdatsenko <dacenko.aa@yandex.ru>
2024-07-13 08:33:56 +0300

    test

--- app.py
+++ app.py
@@ -58,7 +58,8 @@ def take_info():
 @app.route('/test', methods=['POST'])
 def func():
     data = request.get_json()
-    send_data(chat_id=int(data['chat_id']), data=data['data'])
+    data = json.loads(data)
+    send_data(chat_id=int(data['chat_id']), data=data)
     return jsonify({"status": "success"})
 
 
